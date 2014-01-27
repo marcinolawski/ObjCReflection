3e02c9539d3bbf5673f109e5cd8ca9f9bb8625d5
Marcin <marcin.olawski@gmail.com>
2014-01-27 19:31:00 +0100

    Initial Commit

---
+++ ObjCReflection/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjCReflection
+//
+//  Created by Marcin Olawski on 27.01.2014.
+//  Copyright (c) 2014 Marcin Olawski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
